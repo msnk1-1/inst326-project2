ebef653071f7199319ee27afbcf1558cdcccfb34
msnk1-1 <messiah.khalfani3@gmail.com>
2025-11-02 21:10:08 -0500

    Update ir_library.py

--- src/ir_library.py
+++ src/ir_library.py
@@ -1,1 +1,20 @@
+# Functional Information Retrieval Library
+
+def tokenize(text, stopwords=None):
+    if stopwords is None:
+        stopwords = {"the", "is", "and", "in", "a", "to", "of"}
+    return [w.lower() for w in text.split() if w.lower() not in stopwords]
+
+def build_inverted_index(documents, stopwords=None):
+    index = {}
+    for doc_id, text in documents.items():
+        tokens = tokenize(text, stopwords)
+        for token in tokens:
+            if token not in index:
+                index[token] = set()
+            index[token].add(doc_id)
+    return index
+
+def search(index, term):
+    return index.get(term.lower(), set())
 
